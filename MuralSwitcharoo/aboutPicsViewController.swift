46ae3c033184dc97bd996ac26449dea4764b57c5
Alex <amasztak3281@stu.d214.org>
2022-04-28 12:33:37 -0500

    added Thread sleep timer to app delegate + edited images for tab bar

---
+++ MuralSwitcharoo/aboutPicsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  aboutPicsViewController.swift
+//  MuralSwitcharoo
+//
+//  Created by Alexander Masztak on 4/28/22.
+//
+
+import UIKit
+
+class aboutPicsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
