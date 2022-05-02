076b059a241330aab7c26d6bee07df0a03ab0fa1
Alex <amasztak3281@stu.d214.org>
2022-05-02 13:30:58 -0500

    point.y changed for pic move

--- MuralSwitcharoo/pictureViewController.swift
+++ MuralSwitcharoo/pictureViewController.swift
@@ -18,25 +18,30 @@ class pictureViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    //goes off the screen at point.x = 122. Fix
     
     @IBAction func pic1move(_ sender: UIPanGestureRecognizer) {
         var point = sender.location(in: view)
-        pic1.center = CGPoint(x: point.x, y: point.y)
+        self.view.bringSubviewToFront(pic1)
+        pic1.center = CGPoint(x: point.x, y: 430)
     }
     
     @IBAction func pic2move(_ sender: UIPanGestureRecognizer) {
         var point = sender.location(in: view)
-        pic2.center = CGPoint(x: point.x, y: point.y)
+        self.view.bringSubviewToFront(pic2)
+        pic2.center = CGPoint(x: point.x, y: 430)
     }
     
     @IBAction func pic3move(_ sender: UIPanGestureRecognizer) {
         var point = sender.location(in: view)
-        pic3.center = CGPoint(x: point.x, y: point.y)
+        self.view.bringSubviewToFront(pic3)
+        pic3.center = CGPoint(x: point.x, y: 430)
     }
     
     @IBAction func pic4move(_ sender: UIPanGestureRecognizer) {
         var point = sender.location(in: view)
-        pic4.center = CGPoint(x: point.x, y: point.y)
+        self.view.bringSubviewToFront(pic4)
+        pic4.center = CGPoint(x: point.x, y: 430)
     }
     
 }
