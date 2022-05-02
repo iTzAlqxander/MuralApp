918dfc492b4fe0398b947b8930581102fb3caeb9
Alex <amasztak3281@stu.d214.org>
2022-05-02 12:45:12 -0500

    gestures

--- MuralSwitcharoo/pictureViewController.swift
+++ MuralSwitcharoo/pictureViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class pictureViewController: UIViewController {
 
+    @IBOutlet weak var pic1: UIImageView!
+    @IBOutlet weak var pic2: UIImageView!
+    @IBOutlet weak var pic3: UIImageView!
+    @IBOutlet weak var pic4: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
