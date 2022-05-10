1880a1bc6c532001d287eceba1c40c7362a786b9
Alex Masztak <itzalqxander@gmail.com>
2022-05-10 14:01:05 -0500

    test2

--- MuralSwitcharoo/aboutPicsViewController.swift
+++ MuralSwitcharoo/aboutPicsViewController.swift
@@ -24,7 +24,7 @@ class aboutPicsViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-     test2
+     test3
     */
 
 }
